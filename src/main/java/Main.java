96dbe0bc3680ab2bb3e056db76630653eeefa4f9
Duncan Mackay <37164192+TwistedNoseHair@users.noreply.github.com>
2018-11-01 17:48:07 +1100

    Fix master and tidy up main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,17 +9,7 @@ public class Main {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		new MainController();
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					//new Controller();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
+		new MainController();		
 	}
 }
 
