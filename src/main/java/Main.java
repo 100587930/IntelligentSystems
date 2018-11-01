514c87c8582cf0f70d3f231206763a9d9a3c27ed
Duncan Mackay <37164192+TwistedNoseHair@users.noreply.github.com>
2018-11-01 13:54:57 +1100

    Revert "Revert "Merge branch 'Gui'""
    This reverts commit 85579c5b45b7612e87ad6dd7cf97b16304aac32a.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.awt.EventQueue;
 import controllers.MainController;
-import Gui.gui;
+import Gui.View;
+
 
 public class Main {
 
@@ -9,17 +10,16 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		new MainController();
-
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					gui window = new gui();
-					window.frame.setVisible(true);
+					//new Controller();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-
+		
 	}
 }
+
