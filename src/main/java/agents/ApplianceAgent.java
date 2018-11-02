4d9c683b7e00a3b6e03b4a4569319f4419cac2f9
Duncan Mackay <37164192+TwistedNoseHair@users.noreply.github.com>
2018-11-02 15:43:56 +1100

    merge

--- src/main/java/agents/ApplianceAgent.java
+++ src/main/java/agents/ApplianceAgent.java
@@ -3,11 +3,29 @@ package agents;
 import behaviours.ApplianceCyclicBehaviour;
 import behaviours.ApplianceTickerBehaviour;
 import behaviours.RetailerCyclicBehaviour;
+import forecasters.Forcaster;
+import models.Appliance;
+import models.Retailer;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 import jade.core.*;
 import jade.lang.acl.ACLMessage;
+import utils.Constants;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
+import behaviours.HomeCyclicBehaviour;
+import jade.core.AID;
 import jade.core.Agent;
-
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
+import jade.util.leap.ArrayList;
+import models.Appliance;
+import models.Retailer;
 import utils.Constants;
 
 @SuppressWarnings("serial")
@@ -17,18 +35,49 @@ public class ApplianceAgent extends Agent {
 	private float energyUsage;
 	private AID homeAgent = Constants.HOME_AGENT_AID;
 	private long mSecondsToInform = 3000; // change for different frequencies
-
+	private HashMap<AID, Appliance> applianceAgents = new HashMap<AID, Appliance>();
+	private Forcaster energyforcaster;
 	protected void setup() {
 		this.subscribe();
-		this.setExpectedUsage((float)Math.random()*50);
-		//this.setExpectedUsage(15); // TODO: CHANGE HERE FOR THE FORECASTING
+		
+		for (Entry<AID, Appliance> entry : this.applianceAgents.entrySet()) {
+			int iend = entry.getKey().getName().indexOf("@");
+			String CurrentAgent = entry.getKey().getName().substring(0, iend);
+			
+			if(CurrentAgent == "Dishwasher"){
+				energyforcaster = new Forcaster("DWE");
+				
+			}
+			else if(CurrentAgent == "Heater") {
+				
+				energyforcaster = new Forcaster("FRE");
+			}
+			else if(CurrentAgent == "Fridge") {
+				
+				energyforcaster = new Forcaster("FGE");
+			}
+			else if(CurrentAgent == "TV") {
+				
+				energyforcaster = new Forcaster("TVE");
+			}
+			else if(CurrentAgent == "Washing Machine") {
+				
+				energyforcaster = new Forcaster("CWE");
+			}
+			
+		}
+		
+		
+		
+		this.setExpectedUsage(energyforcaster.getPrediction()); 
+
 		addBehaviour(new ApplianceTickerBehaviour(this, this.mSecondsToInform));
 		addBehaviour(new ApplianceCyclicBehaviour(this));
+		
 	}
 
 	public String getExpectedUsage() {
-		//return Float.toString(this.energyExpected);
-		return Float.toString((float)Math.random()*50);
+		return Float.toString(this.energyExpected);
 	}
 
 	public void setExpectedUsage(float energyUsage) {
@@ -52,8 +101,7 @@ public class ApplianceAgent extends Agent {
 	}
 
 	public float getEnergyUsage() {
-		// TODO: change it later with the real usage
-		this.energyUsage = this.energyExpected;
+		this.energyUsage = energyforcaster.getPrediction();
 		return this.energyUsage;
 	}
 
