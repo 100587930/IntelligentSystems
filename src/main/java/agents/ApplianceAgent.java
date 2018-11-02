3c3c8b04ffb50381ef53fe800bf2edc28245f1f1
Shaun Elders <100587930@student.swin.edu.au>
2018-11-02 15:07:09 +1100

    Revert "Merge branch 'master' into Weka"
    This reverts commit bab7e5621d93b541db2599782a6e518659daf1d5, reversing
    changes made to 48cc1d106562b9f7e448862d0cc8134adf7fb099.

--- src/main/java/agents/ApplianceAgent.java
+++ src/main/java/agents/ApplianceAgent.java
@@ -70,9 +70,12 @@ public class ApplianceAgent extends Agent {
 		
 		
 		this.setExpectedUsage(energyforcaster.getPrediction()); 
+<<<<<<< HEAD
 
 
 
+=======
+>>>>>>> parent of bab7e56... Merge branch 'master' into Weka
 		addBehaviour(new ApplianceTickerBehaviour(this, this.mSecondsToInform));
 		addBehaviour(new ApplianceCyclicBehaviour(this));
 		
